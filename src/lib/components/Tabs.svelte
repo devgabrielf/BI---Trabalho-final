754a4d349bb2a8144c1aabddf93361c67b174247
Gabriel Ferreira <gabriel.ferreira5584@gmail.com>
2023-11-18 18:23:52 -0300

    fix: adjusting layout

--- src/lib/components/Tabs.svelte
+++ src/lib/components/Tabs.svelte
@@ -5,10 +5,10 @@
 </script>
 
 <div
-	class="fixed left-0 right-0 top-[50px] flex w-screen overflow-x-auto bg-zinc-900 md:relative md:left-[unset] md:right-[unset] md:top-[unset] md:w-[calc(100vw-250px)]"
+	class="fixed left-0 min-h-[40px] right-0 top-[50px] flex w-screen overflow-x-auto bg-zinc-900 md:relative md:left-[unset] md:right-[unset] md:top-[unset] md:w-[calc(100vw-250px)]"
 >
 	{#each pages as page (page.route)}
 		<Tab {page} />
 	{/each}
 </div>
-<div class="h-[50px] md:hidden" />
+<div class="h-[40px] md:hidden" />
--- src/routes/powerbi/+page.svelte
+++ src/routes/powerbi/+page.svelte
@@ -7,11 +7,13 @@
 	<link rel="icon" type="image/svg" href={jsIcon} />
 </svelte:head>
 
-<iframe
-	title="Trabalho final de BI"
-	width="100%"
-	height="100%"
-	src="https://app.powerbi.com/reportEmbed?reportId=e810e772-9bc4-4831-acfd-89288f5e5fd3&autoAuth=true&ctid=14cbd5a7-ec94-46ba-b314-cc0fc972a161"
-	frameborder="0"
-	allowfullscreen
-/>
+<div class="h-full min-h-[600px]">
+	<iframe
+		title="Trabalho final de BI"
+		width="100%"
+		height="100%"
+		src="https://app.powerbi.com/reportEmbed?reportId=e810e772-9bc4-4831-acfd-89288f5e5fd3&autoAuth=true&ctid=14cbd5a7-ec94-46ba-b314-cc0fc972a161"
+		frameborder="0"
+		allowfullscreen
+	/>
+</div>
