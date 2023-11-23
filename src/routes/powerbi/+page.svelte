a4774d1b2c4d181ab314d6c9a7a801525f200040
Gabriel Ferreira <gabriel.ferreira5584@gmail.com>
2023-11-23 19:41:47 -0300

    feat: adding link to powerbi

--- src/routes/powerbi/+page.svelte
+++ src/routes/powerbi/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { ExternalLink } from "lucide-svelte";
+
 	import jsIcon from "$assets/js.svg";
 </script>
 
@@ -7,13 +9,22 @@
 	<link rel="icon" type="image/svg" href={jsIcon} />
 </svelte:head>
 
-<div class="h-full min-h-[600px]">
-	<iframe
-		title="Trabalho final de BI"
-		width="100%"
-		height="100%"
-		src="https://app.powerbi.com/reportEmbed?reportId=e810e772-9bc4-4831-acfd-89288f5e5fd3&autoAuth=true&ctid=14cbd5a7-ec94-46ba-b314-cc0fc972a161"
-		frameborder="0"
-		allowfullscreen
-	/>
+<div class="flex h-full min-h-[600px] w-full flex-col gap-4">
+	<a
+		href="https://app.powerbi.com/reportEmbed?reportId=e810e772-9bc4-4831-acfd-89288f5e5fd3&autoAuth=true&ctid=14cbd5a7-ec94-46ba-b314-cc0fc972a161"
+		target="_blank"
+		class="self-end"
+	>
+		<ExternalLink color="#aaa" /></a
+	>
+	<div class="w-full flex-1">
+		<iframe
+			title="Trabalho final de BI"
+			width="100%"
+			height="100%"
+			src="https://app.powerbi.com/reportEmbed?reportId=e810e772-9bc4-4831-acfd-89288f5e5fd3&autoAuth=true&ctid=14cbd5a7-ec94-46ba-b314-cc0fc972a161"
+			frameborder="0"
+			allowfullscreen
+		/>
+	</div>
 </div>
--- src/routes/referencias/+page.svelte
+++ src/routes/referencias/+page.svelte
@@ -11,7 +11,7 @@
 <ul class="flex list-disc flex-col gap-3 p-8">
 	{#each universities as university (university.Universidade)}
 		{@const universityName = university.Universidade}
-		{@const city = university["Endereço"].split("-").at(-2)?.split(", ").at(-1)}
+		{@const city = university["Endereço"].split(" -").at(-2)?.split(", ").at(-1)}
 		<li class="text-zinc-200">
 			INEP. Enade 2021 - {universityName} - {city}. Brasília, 2021.
 		</li>
